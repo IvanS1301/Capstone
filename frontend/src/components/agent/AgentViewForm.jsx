65550307213173adba624cd26cfe6cb553056007
giguia <154893878+giguia@users.noreply.github.com>
2024-07-17 18:19:51 +0800

    skype

--- frontend/src/components/agent/AgentViewForm.jsx
+++ frontend/src/components/agent/AgentViewForm.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useLeadsContext } from "../../hooks/useLeadsContext";
-import { Container, Typography, Box, Paper, Grid } from '@mui/material';
+import { Container, Typography, Box, Paper, Grid, IconButton } from '@mui/material';
 import moment from 'moment';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const AgentViewForm = ({ unassignedId }) => {
   const { unassignedLeads } = useLeadsContext();
@@ -31,97 +31,139 @@ const AgentViewForm = ({ unassignedId }) => {
   };
 
   return (
-    <Container>
-      <Paper elevation={3} sx={{ padding: 5, borderRadius: 6, boxShadow: '1px 1px 8px rgba(0, 0, 0, 0.065)', backgroundColor: '#041926' }}>
-        <Box mb={3} textAlign="center" style={{ backgroundColor: '#9c1c1c', padding: '3px', borderTopLeftRadius: '6px', borderTopRightRadius: '6px', marginTop: '4px' }}>
-          <Typography variant="h4" component="h1" gutterBottom style={{ color: '#e0e0e0', borderBottomLeftRadius: '6px', borderBottomRightRadius: '6px', marginTop: '10px' }}>
-            {lead.name}
-          </Typography>
-        </Box>
-
+    <Container >
+      <Paper elevation={3} 
+      sx={{ padding: 0, borderRadius: 5, boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.065)',paddingTop: '0', backgroundColor: '#fff7ed'  }}>
+      <Box
+      mb={3}
+      textAlign="center"
+      style={{
+        backgroundColor: '#111827',
+        padding: '3px',
+        borderTopLeftRadius: '6px',
+        borderTopRightRadius: '6px',
+        marginTop: '4px',
+        position: 'relative', // Ensure the Box is relative for absolute positioning
+      }}
+    >
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        style={{
+          color: '#e0e0e0',
+          borderBottomLeftRadius: '6px',
+          borderBottomRightRadius: '6px',
+          marginTop: '10px',
+          display: 'inline-block',
+          fontFamily: 'Century Gothic, sans-serif',
+        }}
+      >
+        {lead.name}
+      </Typography>
+      <IconButton
+        href={`skype:${lead.phonenumber}?call`}
+        style={{
+          position: 'absolute',
+          right: '10px', // Adjust this value to move the icon horizontally
+          top: '12px', // Adjust this value to move the icon vertically
+          color: 'white',
+        }}
+      >
+        <FontAwesomeIcon icon={faPhone} />
+      </IconButton>
+    </Box>
+        <div>
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Type: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.type}</Typography>
+          <Grid item xs={12} sm={12}>
+            <Typography variant="body1" component="p" fontSize="20px" 
+            style={{ 
+              color: '#020617', 
+              marginLeft: '30px'}}>
+            <span style={{ color: '#020617' }}><strong>Type: </strong></span>{lead.type} </Typography>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Phone Number: </strong></Typography>
+          <Grid item xs={12} sm={12}>
             <Box display="flex" alignItems="center">
-              <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.phonenumber}</Typography>
+            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#020617', marginLeft: '30px', }}>
+            <span style={{ color: '#020617' }}><strong>Phone Number: </strong></span>{lead.phonenumber} </Typography>
               <CopyToClipboard text={lead.phonenumber} onCopy={() => handleCopy('phone')}>
-                <button style={{ marginLeft: '10px', backgroundColor: 'transparent', border: 'none', cursor: 'pointer', color: 'white', fontSize: '20px' }}>
+                <button style={{ marginLeft: '10px', backgroundColor: 'transparent', border: 'none', cursor: 'pointer', color: '#020617', fontSize: '20px' }}>
                   <FontAwesomeIcon icon={faCopy} />
                 </button>
               </CopyToClipboard>
               {clipboardState.phone && (
                 <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'green', marginLeft: '10px' }}>
-                  copied
+                  copied to clipboard
                 </Typography>
               )}
             </Box>
+            
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Street Address: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.streetaddress}</Typography>
+          <Grid item xs={12} sm={12}>
+            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#020617', marginLeft: '30px', }}>
+            <span style={{ color: '#020617' }}><strong>Address: </strong></span>{lead.streetaddress} {lead.city} {lead.postcode}</Typography>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>City: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.city}</Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Postcode: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.postcode}</Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Email Address: </strong></Typography>
-            <Box display="flex" alignItems="center">
-              <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.emailaddress}</Typography>
-              <CopyToClipboard text={lead.emailaddress} onCopy={() => handleCopy('email')}>
-                <button style={{ marginLeft: '10px', backgroundColor: 'transparent', border: 'none', cursor: 'pointer', color: 'white', fontSize: '20px' }}>
+          <Grid item xs={12} sm={12}>
+          <Box display="flex" alignItems="center">
+            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#020617', marginLeft: '30px', }}>
+            <span style={{ color: '#020617' }}><strong>Email Address: </strong></span>{lead.emailaddress}</Typography>
+              <CopyToClipboard text={lead.emailaddress}onCopy={() => handleCopy('email')}>
+                <button style={{ 
+                  marginLeft: '10px', backgroundColor: 'transparent', border: 'none', cursor: 'pointer', color: '#020617', fontSize: '20px' }}>
                   <FontAwesomeIcon icon={faCopy} />
                 </button>
               </CopyToClipboard>
               {clipboardState.email && (
                 <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'green', marginLeft: '10px' }}>
-                  copied
+                  copied to clipboard
                 </Typography>
               )}
             </Box>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Call Disposition: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.callDisposition}</Typography>
+          <Grid item xs={12} sm={12}>
+            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#020617', marginLeft: '30px', }}>
+            <span style={{ color: '#020617' }}><strong>Call Disposition: </strong></span>{lead.callDisposition}</Typography>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Remarks: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{lead.remarks}</Typography>
+          <Grid item xs={12} sm={12}>
+            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#020617', marginLeft: '30px', }}>
+            <span style={{ color: '#020617' }}><strong>Remarks: </strong></span>{lead.remarks}</Typography>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Lead Gen Date: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{formattedCreatedAt}</Typography>
+          <Grid item xs={12} sm={12}>
+            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#020617', marginLeft: '30px' }}>
+            <span style={{ color: '#020617' }}><strong>Lead Gen Date: </strong></span>{formattedCreatedAt}</Typography>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Distributed: </strong></Typography>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{formattedDistributed}</Typography>
+          <Grid item xs={12} sm={12}>
+            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#020617', marginLeft: '30px'}}>
+            <span style={{ color: '#020617' }}><strong>Distributed: </strong></span>{formattedDistributed}</Typography>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body1" component="p" fontSize="20px" style={{ color: '#94e2cd' }}><strong>Last Touch: </strong></Typography>
-            {lead.callDisposition && (
-              <Typography variant="body1" component="p" fontSize="20px" style={{ color: 'white' }}>{formattedUpdatedAt}</Typography>
-            )}
+          <Grid item xs={12} sm={12}>
+            <Typography 
+              variant="body1" 
+              component="p" 
+              fontSize="20px" 
+              style={{ 
+                color: '#020617', 
+                marginLeft: '30px',  // Add your desired left margin here
+              }}>
+              <span style={{ color: '#020617' }}>
+                <strong>Last Touch: </strong>
+              </span>
+              {formattedUpdatedAt}
+            </Typography>
           </Grid>
+
+          
+          
+        
+          {/* <IconButton 
+            href={`skype:${lead.phonenumber}?call`} 
+            style={{ marginLeft: '10px', color: '#020617' }}>
+            <FontAwesomeIcon icon={faPhone} />
+          </IconButton> */}
         </Grid>
-
-        {/* Logo and company name */}
-        <Box display="flex" justifyContent="flex-end" alignItems="center">
-          <img
-            src={process.env.PUBLIC_URL + '/logo.png'}
-            alt="logo"
-            style={{ width: '50px', height: '50px', marginRight: '8px' }}
-          />
-          <Typography variant="h6" component="h2" style={{ color: '#e0e0e0', fontSize: '1.7rem', marginTop: '2px' }}>Chromagen</Typography>
-        </Box>
-
+        </div>
+        <br/>
+        <br/>
       </Paper>
     </Container>
   );
